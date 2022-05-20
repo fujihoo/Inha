49a2ae28f6dd074b47e349df7e26975c9dca9910
PARK JIHOO <fujihoo@naver.com>
2022-05-20 18:17:13 +0900

    i am done.

---
+++ API_Work/2.CppProj/202205/20220520/20220520.cpp
@@ -0,0 +1,227 @@
+﻿#define	_CRT_SECURE_NO_WARNINGS
+//#include <iostream>
+//#include <string>
+//
+//using namespace std;
+//struct Pizza
+//{
+//	string sName;
+//	double dDia;
+//	double dWeight;
+//};
+//
+//int main()
+//{
+//	Pizza infor;
+//
+//	cout << "피자 회사의 이름을 입력하시오.";
+//	cin >> infor.sName;
+//	cout << "피자의 지름을 입력하시오.";
+//	cin >> infor.dDia;
+//	cout << "피자의 중량을 입력하시오.";
+//	cin >> infor.dWeight;
+//
+//	
+//	cout << "피자 회사의 이름: " << infor.sName
+//		<< "피자의 지름을 입력하시오: " << infor.dDia
+//		<< "피자의 중량을 입력하시오: " << infor.dWeight;
+//
+//	return 0;
+//}
+
+//#include <iostream>
+//#include <string>
+//
+//using namespace std;
+//struct Pizza
+//{
+//	string sName;
+//	double dDia;
+//	double dWeight;
+//};
+//
+//int main()
+//{
+//	Pizza* ps = new Pizza;
+//	cout << "피자 회사의 이름을 입력하시오.";
+//	cin >> (*ps).sName;
+//	cout << "피자의 지름을 입력하시오.";
+//	cin >> (*ps).dDia;
+//	cout << "피자의 중량을 입력하시오.";
+//	cin >> (*ps).dWeight;
+//
+//	cout << "피자 회사의 이름: " << (*ps).sName << endl
+//	<< "피자의 지름을 입력하시오: " << (*ps).dDia << "cm" << endl
+//		<< "피자의 중량을 입력하시오: " << (*ps).dWeight << "g" << endl;
+//	delete ps;
+//
+//	return 0;
+//}
+
+//Q2. 단어를 입력받아서 그 단어를 뒤집었을 때 원래의
+//    단어와 같은지를 판단하는 프로그램을 작성하라.
+//    ex) 단어를 입력하시오 : level
+//        level은 회문입니다.
+//        단어를 입력하시오 : animal
+//        animal은 회문이 아닙니다.
+
+//#include	<iostream>
+//#include	<string>
+//
+//int main()
+//{
+//	using namespace std;
+//	string word1;
+//	string word2;
+//
+//	cout << "단어를 입력하시오 : ";
+//	cin >> word1;
+//	word2 = word1;
+//
+//	//문자열을 거꾸로 만들기
+//	for (int i = word2.size() - 1; i >= 0; i--)
+//	{
+//		word2[i] = word1[word2.size() - 1 - i];
+//	}
+//	if (word1 == word2)
+//	{
+//		cout << word1 << "은 회문입니다." << endl;
+//	}
+//	else
+//	{
+//		cout << word1 << "은 회문이 아닙니다." << endl;
+//	}
+//
+//	return 0;
+//}
+
+//#include <iostream>
+//
+//int main()
+//{
+//	clock_t start = clock();
+//	clock_t delay = cl
+//}
+
+//#include <iostream>
+//
+//int main()
+//{
+//	using namespace std;
+//	int prices[5] = { 1, 3, 4, 5, 6 };
+//
+//	int sum = 0;
+//	for (int i : prices)
+//	{
+//		sum += i;
+//	}
+//	cout << sum << endl;
+//
+//	return 0;
+//}
+
+
+//#include <iostream>
+//
+//int main()
+//{
+//	using namespace std;
+//
+//	char ch, ch2;;
+//	cin.get(ch);
+//	cin.get();
+//	cin.get(ch).get();
+//	ch = cin.get()
+//	cin.get(ch).get(ch2);
+//
+//	return 0;
+//}
+
+//Q3. 1~10까지의 정수를 입력받아 다음 피라미드와
+//    같은 형태로 출력하는 프로그램을 작성하라.
+//    ex) 라인 숫자를 입력 : 5
+//         1
+//        212
+//       32123
+//      4321234
+//     543212345
+
+//#include <iostream>
+//using namespace std;
+//
+//void pira(int iN);
+//
+//int main()
+//{
+//	cout << "숫자를 입력하세요" << endl;
+//	int iN = 0;
+//	cin >> iN;
+//
+//	pira(iN);
+//
+//	return 0;
+//}
+//
+//void pira(int iN)
+//{
+//	for (int i = 0; i < iN; i++)
+//	{
+//		for (int j = 0; j < iN - i - 1; j++)
+//		{
+//			cout << " ";
+//		}
+//		for (int k = i + 1; k > 0; k--)
+//		{
+//			cout << k;
+//		}
+//		for (int l = 2; l < i + 2; l++)
+//		{
+//			cout << l;
+//		}
+//		if (i != iN - 1)
+//		{
+//			cout << "\n";
+//		}
+//	}
+//}
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//struct CarModel
+//{
+//	char cName[128];
+//	int iYear;
+//};
+//
+//int main()
+//{
+//	int number = 0;
+//
+//	cout << "몇대의 차를 목록으로 관리하겠습니까? ";
+//	cin >> number;
+//	CarModel* ps = new CarModel[number];
+//
+//	for (int i = 0; i < number; i++)
+//	{
+//		cout << "자동차 #" << (i + 1) << ":" << endl;
+//		cout << "제작업체: ";
+//		cin >> (*(ps + i)).cName;
+//		cout << "제작년도: ";
+//		cin >> (*(ps + i)).iYear;
+//	}
+//
+//	cout << "현재 귀하가 보유하고 있는 자동차 목록은 다음과 같습니다." << endl;
+//
+//	for (int i = 0; i < number; i++)
+//	{
+//		cout << (*(ps + i)).iYear << "년형 " << (*(ps + i)).cName << endl;
+//	}
+//
+//	return 0;
+//}
+
+#include <iostream>
+
+using namespace std;
---
+++ API_Work/3.Algorithm/202205/20220520/20220520.cpp
@@ -0,0 +1,75 @@
+﻿//Q. 10진수를 입력받아 다른 진수로 변환하는 프로그램을 작성하라.
+//   (2~36진수)
+
+#define	_CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+int Antilogalithm_Change(int iDecimal, int iAntilogalithm);
+
+int main()
+{
+
+	int iDecimal = 0;
+	int iAntilogalithm = 0;
+	char iUp10;
+
+	cout << "다른 진수로 변환할 10진수를 입력하시오: ";
+	cin >> iDecimal;
+	cout << "몇 진수로 변환 하시겠습니까?: ";
+	cin >> iAntilogalithm;
+
+	if (iDecimal >= 0 && iAntilogalithm >= 2 && iAntilogalithm <= 36)
+	{
+		Antilogalithm_Change(iDecimal, iAntilogalithm);
+	}
+
+	return 0;
+}
+
+int Antilogalithm_Change(int iDecimal, int iAntilogalithm)
+{
+	if (iAntilogalithm > 9)
+	{
+		if (iDecimal != 0 && iDecimal != 1)
+		{
+			Antilogalithm_Change(iDecimal / iAntilogalithm, iAntilogalithm);
+			if (iDecimal % iAntilogalithm >= 10)
+			{
+				cout << (char)((iDecimal % iAntilogalithm) - 10 + 'A');
+			}
+			else
+			{
+				cout << iDecimal % iAntilogalithm;
+			}
+		}
+	}
+	else
+	{
+		if (iDecimal == 0 || iDecimal == 1)
+		{
+			cout << iDecimal;
+		}
+		else
+		{
+			Antilogalithm_Change(iDecimal / iAntilogalithm, iAntilogalithm);
+			if (iDecimal % iAntilogalithm >= 10)
+			{
+				cout << (char)((iDecimal % iAntilogalithm) - 10 + 'A');
+			}
+			else
+			{
+				cout << iDecimal % iAntilogalithm;
+			}
+		}
+	}
+
+	return 1;
+}
+
+//Q. 각 진수끼리 변환하는 프로그램을 작성하라.
+//   (2~36진수 사이로)
+//   ex) 변환할 데이터의 진수는 : 2
+//       데이터를 입력하시오 : 101
+//       출력할 데이터의 진수는 : 10
+//       변환된 데이터는 : 5
