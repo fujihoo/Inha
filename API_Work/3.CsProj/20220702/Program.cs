a2ea53581aed296b5d88081691ff10491d0a1999
PARK JIHOO <fujihoo@naver.com>
2022-07-02 17:55:48 +0900

    Hello, World!

---
+++ API_Work/3.CsProj/20220702/Program.cs
@@ -0,0 +1,22 @@
+﻿//using System;
+
+//namespace _20220702
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Hello World!");
+//        }
+//    }
+//}
+
+using System;
+
+class Hello
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World");
+    }
+}
